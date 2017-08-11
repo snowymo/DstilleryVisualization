a0cc7e5e9a84311e81fc9cfc20f5331dd4aad7ad
Zhenyi He <snowymohehe@gmail.com>
2017-08-10 22:21:13 -0400

    create a mesh containing up to 65000 vertices to represent around 8000 cubes

---
+++ Assets/Scripts/MeshGen.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshGen : MonoBehaviour {
+
+	public GameObject prefab;
+
+	public Vector3 pos1;
+	public Vector3 pos2;
+
+	MyMesh mm;
+
+	// Use this for initialization
+	void Start () {
+		// which does not work because too many objects
+//		for (int i = 0; i < 10000; i++) {
+//			GameObject.Instantiate (prefab, new Vector3 (Random.Range (0, 10), Random.Range (0, 10), Random.Range (0, 10)),Quaternion.identity);
+//		}
+		mm = new MyMesh();
+		// 65000 vertices at most
+		for (int i = 0; i < 4000; i++) {
+			mm.addOneObj (pos1);
+			mm.addOneObj (pos2);
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Mesh mesh = GetComponent<MeshFilter>().mesh;
+		mesh.vertices = mm.vertices;
+		mesh.uv = mm.uvs;
+		mesh.triangles = mm.triangles;
+		mesh.normals = mm.norms;
+	}
+}
